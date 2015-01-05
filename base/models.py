be24c0a91171d2521e468d806aa05cb566342b53
kudrom <kudrom@riseup.net>
2015-01-05 13:41:53 +0100

    modelo actualizado

--- base/models.py
+++ base/models.py
@@ -1,48 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class Clientes(models.Model)
-	id_cliente = models.AutoField(primary_key=True)
-	nombre = models.CharField(max_length=45)
-	primer_apellido = models.CharField(max_length=50)
-	segundo_apellido = models.CharField(max_length=50)
+ESTADOS = (
+        ('SO', 'Solicitada'),
+        ('AC', 'Aceptada'),
+        ('AS', 'Asignada'),
+        ('CP', 'Cerrada prematuramente'),
+        ('CE', 'Cerrada con solución'),
+        ('CF', 'Cerrada sin solución')
+    )
 
-class Inventario(models.Model)
-	id_item = models.AutoField(primary_key=True)
-	precio_compra = models.FloatField()
-	fecha_compra = models.DateField()
-	tipo = models.CharField(max_length=8)
-	caracteristicas = models.CharField(max_length=200)
+class ElementoInventario(models.Model):
+    precio_compra = models.FloatField()
+    fecha_compra = models.DateField()
+    tipo = models.CharField(max_length=8)
+    caracteristicas = models.CharField(max_length=200)
 
-class Empleados(models.Model)
-	id_empleado = models.AutoField(primary_key=True)
-	nombre = models.CharField(max_length=45)
-	primer_apellido = models.CharField(max_length=50)
-	segundo_apellido = models.CharField(max_length=50)
-	puesto = models.CharField(max_length=12)
+class Incidencia(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=300)
+    prioridad = models.IntegerField()
+    categoria = models.CharField(max_length=80)
+    estado = models.CharField(max_length=2, choices=ESTADOS, default='SO')
+    inventario = models.ForeignKey(ElementoInventario)
 
-class Comentarios(models.Model)
-	id_comentario = models.AutoField(primary_key=True)
-	texto = models.CharField(max_length=200)
-	id_empleado = models.ForeignKey(Empleados)
+    fecha_apertura = models.DateField()
+    fecha_cierre = models.DateField()
+    resuelta_exito = models.BooleanField(default=False)
 
-class Notificaciones(models.Model)
-	id_notificacion = models.AutoField(primary_key=True)
-	texto = models.CharField(max_length=100)
-	id_empleado = models.ForeignKey(Empleados)
+    tecnico_asignado = models.ForeignKey(User, null=True, related_name="tecnico_asignado")
+    supervisor = models.ForeignKey(User, null=True, related_name="supervisor")
+    cliente = models.ForeignKey(User, null=True, related_name="cliente")
 
-class Incidencias(models.Model):
-	id_incidencia = models.AutoField(primary_key=True)
-	nombre_incidencia = models.CharField(max_length=100)
-	estado = models.CharField(max_length=22)
-	fecha_apertura = models.DateField()
-	fecha_cierre = models.DateField()
-	descripcion = models.CharField(max_length=300)
-	prioridad = models.IntegerField()
-	categoria = models.CharField(max_length=80)
-	resolucion = models.CharField(max_length=2)
-	id_cliente = models.ForeignKey(Clientes)
-	id_comentario = models.ForeignKey(Comentarios)
-	id_item = models.ForeignKey(Inventario)
-	id_empleado = models.ForeignKey(Empleados)
-	id_notificacion = models.ForeignKey(Notificaciones)
-
+class CambioEstado(models.Model):
+    incidencia = models.ForeignKey(Incidencia)
+    usuario = models.ForeignKey(User)
+    fecha_cambio = models.DateField()
+    estado_inicial = models.CharField(max_length=2, choices=ESTADOS, default='SO')
+    estado_final = models.CharField(max_length=2, choices=ESTADOS, default='AC')
+    nuevo = models.BooleanField(default=True)
